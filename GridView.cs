ba154bc39d46e5c26a059e753a9929f875181179
morrisscd <kryten2q4b@hotmail.co.uk>
2016-10-15 12:09:02 +0100

    Add project files.

---
+++ GridView.cs
@@ -0,0 +1,307 @@
+﻿using System;
+//using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Data;
+using System.Windows.Shapes;
+using System.Windows.Media;
+using System.Globalization;
+using System.Windows.Input;
+using CSharpExplorerWPF;
+
+namespace fileExplorer
+{
+    public partial class LoadDrivesDirs : Window
+    {
+        public StackPanel CreateStackPanel(object o, bool checkBoxes)
+        {
+            StackPanel sp1 = new StackPanel();
+            sp1.Orientation = Orientation.Horizontal;
+
+            Image folderImage = new Image();
+            folderImage.Source = new BitmapImage
+                (new Uri("C:\\Programming\\CSharpExplorer (WPF)\\FolderImage2.png"));
+            folderImage.Width = 16;
+            folderImage.Height = 16;
+
+            //using textblock as the Label class seems to add some padding
+            TextBlock folderLabel = new TextBlock();
+           
+            folderLabel.VerticalAlignment = System.Windows.VerticalAlignment.Center;
+            folderLabel.Text = o.ToString();
+
+            //CheckBox itemCheckBox = new CheckBox();
+            //if (checkBoxes == true)
+            //    sp1.Children.Add(itemCheckBox);
+
+            sp1.Children.Add(folderImage);
+            sp1.Children.Add(folderLabel);
+            sp1.Tag = "w";
+            return sp1;
+        }
+    }
+
+       
+    //public partial class SingleClickExpand : TreeViewItem, LoadDrivesDirs
+    //{
+    //    //public UserControl1()
+    //    //{
+    //    //    InitializeComponent();
+    //    //}
+
+    //    public static readonly DependencyProperty ExpandNodeProperty =
+    //       DependencyProperty.Register("ExpandNode", typeof(string), typeof(SingleClickExpand), new
+    //          PropertyMetadata("", new PropertyChangedCallback(OnExpandNodeChanged)));
+
+    //    public string ExpandNode
+    //    {
+    //        get { return (string)GetValue(ExpandNodeProperty); }
+    //        set { SetValue(ExpandNodeProperty, value); }
+    //    }
+
+    //    private static void OnExpandNodeChanged(DependencyObject d,
+    //       DependencyPropertyChangedEventArgs e)
+    //    {
+    //        SingleClickExpand obj = d as SingleClickExpand;
+    //        obj.OnExpandNodeChanged(e);
+    //    }
+
+    //    private void OnExpandNodeChanged(DependencyPropertyChangedEventArgs e)
+    //    {
+    //        //int a = 10;
+    //    }
+    //}
+
+    //An attached behaviour
+    //public static class TextBoxBehaviour : LoadDrivesDirs
+    //{
+    //    public static bool GetSelectAll(TreeViewItem target)
+    //    {
+    //        return (bool)target.GetValue(SelectAllAttachedProperty);
+    //    }
+
+    //    public static void SetSelectAll(TreeViewItem target, bool value)
+    //    {
+    //        target.SetValue(SelectAllAttachedProperty, value);
+    //    }
+
+    //    public static readonly DependencyProperty 
+    //        SelectAllAttachedProperty = DependencyProperty.RegisterAttached("SelectAll", 
+    //        typeof(bool), 
+    //        typeof(TextBoxBehaviour), 
+    //        new UIPropertyMetadata(false, OnSelectAllAttachedPropertyChanged));
+
+    //    static void OnSelectAllAttachedPropertyChanged(DependencyObject o, 
+    //        DependencyPropertyChangedEventArgs e)
+    //    {
+    //        //((TreeViewItem)o).SelectAll();
+    //        //OnTreeViewDirExpand(o, e);
+    //        int a = 10;
+    //    }
+
+       
+    //}
+
+
+    public static class MyTreeViewHelper
+    {
+        //
+        // The TreeViewItem that the mouse is currently directly over (or null).
+        //
+        private static TreeViewItem _currentItem = null;
+
+        //
+        // IsMouseDirectlyOverItem:  A DependencyProperty that will be true only on the 
+        // TreeViewItem that the mouse is directly over.  I.e., this won't be set on that 
+        // parent item.
+        //
+        // This is the only public member, and is read-only.
+        //
+
+        // The property key (since this is a read-only DP)
+        private static readonly DependencyPropertyKey IsMouseDirectlyOverItemKey =
+            DependencyProperty.RegisterAttachedReadOnly("IsMouseDirectlyOverItem",
+                                                typeof(bool),
+                                                typeof(MyTreeViewHelper),
+                                                new FrameworkPropertyMetadata(null, 
+                                                new CoerceValueCallback(CalculateIsMouseDirectlyOverItem)));
+
+        // The DP itself
+        public static readonly DependencyProperty IsMouseDirectlyOverItemProperty =
+            IsMouseDirectlyOverItemKey.DependencyProperty;
+
+        // A strongly-typed getter for the property.
+        public static bool GetIsMouseDirectlyOverItem(DependencyObject obj)
+        {
+            return (bool)obj.GetValue(IsMouseDirectlyOverItemProperty);
+        }
+
+        // A coercion method for the property
+        private static object CalculateIsMouseDirectlyOverItem(DependencyObject item, object value)
+        {
+            // This method is called when the IsMouseDirectlyOver property is being calculated
+            // for a TreeViewItem.  
+
+            if (item == _currentItem)
+                return true;
+            else
+                return false;
+        }
+
+        //
+        // UpdateOverItem:  A private RoutedEvent used to find the nearest encapsulating
+        // TreeViewItem to the mouse's current position.
+        //
+
+        private static readonly RoutedEvent UpdateOverItemEvent = EventManager.RegisterRoutedEvent(
+            "UpdateOverItem", RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(MyTreeViewHelper));
+
+        //
+        // Class constructor
+        //
+
+        static MyTreeViewHelper()
+        {
+            // Get all Mouse enter/leave events for TreeViewItem.
+            EventManager.RegisterClassHandler(typeof(TreeViewItem), TreeViewItem.MouseEnterEvent, new MouseEventHandler(OnMouseTransition), true);
+            EventManager.RegisterClassHandler(typeof(TreeViewItem), TreeViewItem.MouseLeaveEvent, new MouseEventHandler(OnMouseTransition), true);
+
+            // Listen for the UpdateOverItemEvent on all TreeViewItem's.
+            EventManager.RegisterClassHandler(typeof(TreeViewItem), UpdateOverItemEvent, new RoutedEventHandler(OnUpdateOverItem));
+        }
+
+
+        //
+        // OnUpdateOverItem:  This method is a listener for the UpdateOverItemEvent.  When it is received,
+        // it means that the sender is the closest TreeViewItem to the mouse (closest in the sense of the tree,
+        // not geographically).
+
+        static void OnUpdateOverItem(object sender, RoutedEventArgs args)
+        {
+            // Mark this object as the tree view item over which the mouse
+            // is currently positioned.
+            _currentItem = sender as TreeViewItem;
+
+            // Tell that item to re-calculate the IsMouseDirectlyOverItem property
+            _currentItem.InvalidateProperty(IsMouseDirectlyOverItemProperty);
+
+            // Prevent this event from notifying other tree view items higher in the tree.
+            args.Handled = true;
+        }
+
+        //
+        // OnMouseTransition:  This method is a listener for both the MouseEnter event and
+        // the MouseLeave event on TreeViewItems.  It updates the _currentItem, and updates
+        // the IsMouseDirectlyOverItem property on the previous TreeViewItem and the new
+        // TreeViewItem.
+
+        static void OnMouseTransition(object sender, MouseEventArgs args)
+        {
+            lock (IsMouseDirectlyOverItemProperty)
+            {
+                if (_currentItem != null)
+                {
+                    // Tell the item that previously had the mouse that it no longer does.
+                    DependencyObject oldItem = _currentItem;
+                    _currentItem = null;
+                    oldItem.InvalidateProperty(IsMouseDirectlyOverItemProperty);
+                }
+
+                // Get the element that is currently under the mouse.
+                IInputElement currentPosition = Mouse.DirectlyOver;
+
+                // See if the mouse is still over something (any element, not just a tree view item).
+                if (currentPosition != null)
+                {
+                    // Yes, the mouse is over something.
+                    // Raise an event from that point.  If a TreeViewItem is anywhere above this point
+                    // in the tree, it will receive this event and update _currentItem.
+
+                    RoutedEventArgs newItemArgs = new RoutedEventArgs(UpdateOverItemEvent);
+                    currentPosition.RaiseEvent(newItemArgs);
+
+                }
+            }
+        }
+
+
+    }
+
+    public static class TreeViewItemExtensions
+    {
+        public static int GetDepth(this TreeViewItem item)
+        {
+            TreeViewItem parent;
+            while ((parent = GetParent(item)) != null)
+            {
+                return GetDepth(parent) + 1;
+            }
+            return 0;
+        }
+
+        private static TreeViewItem GetParent(TreeViewItem item)
+        {
+            var parent = VisualTreeHelper.GetParent(item);
+            while (!(parent is TreeViewItem || parent is TreeView))
+            {
+                parent = VisualTreeHelper.GetParent(parent);
+            }
+            return parent as TreeViewItem;
+        }
+    }
+
+    public class LeftMarginMultiplierConverter : IValueConverter
+    {
+        public double Length { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var item = value as TreeViewItem;
+            if (item == null)
+                return new Thickness(0);
+
+            return new Thickness(Length * item.GetDepth(), 0, 0, 0);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+
+   // [ValueConversion(typeof(bool), typeof(Visibility))]
+    //public class VisibilityConverter : IValueConverter
+    //{
+    //    public const string Invert = "Invert";
+
+    //    #region IValueConverter Members
+
+    //    public object Convert(object value, Type targetType, object parameter,
+    //        System.Globalization.CultureInfo culture)
+    //    {
+    //        if (targetType != typeof(Visibility))
+    //            throw new InvalidOperationException("The target must be a Visibility.");
+
+    //        bool? bValue = (bool?)value;
+
+    //        if (parameter != null && parameter as string == Invert)
+    //            bValue = !bValue;
+
+    //        return bValue.HasValue && bValue.Value ? Visibility.Visible : Visibility.Collapsed;
+    //    }
+
+    //    public object ConvertBack(object value, Type targetType, object parameter,
+    //        System.Globalization.CultureInfo culture)
+    //    {
+    //        throw new NotSupportedException();
+    //    }
+    //    #endregion
+    //}
+      
+
+//Namespace
+}
+
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,342 @@
+﻿using System;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media.Imaging;
+using System.Windows.Data;
+using System.ComponentModel;
+using System.Collections.ObjectModel;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Interop;
+using System.Drawing;
+using System.Runtime.InteropServices;
+
+namespace fileExplorer 
+{
+    public partial class LoadDrivesDirs : Window
+    {   
+        public LoadDrivesDirs()
+        {
+            InitializeComponent();
+
+            DriveInfo[] drives = DriveInfo.GetDrives();
+            DriveInfo defaultDrive = drives[0];
+
+            CreateDriveColumn(drives);
+            CreateNameColumn(defaultDrive.RootDirectory);
+           
+        }
+
+        public void CreateDriveColumn(DriveInfo[] drives)
+        {                                   
+            foreach (DriveInfo driveInfo in drives)
+            {
+                trvStructure.Items.Add(CreateTreeItem(driveInfo));                                   
+            }
+        }
+           
+        private TreeViewItem CreateTreeItem(object o)
+        {
+            TreeViewItem item = new TreeViewItem();
+
+            item.Margin = new Thickness(0, 0, 0, 0);
+            item.Header = CreateStackPanel(o, false);       
+            item.Tag = o ;
+            item.Items.Add("Loading...");
+            //event to handle clicking the expand arrow
+            item.Expanded += new RoutedEventHandler(OnTreeViewDirExpand);
+            //this allows a single mouse click to expand the child tree nodes
+           // item.Selected += new RoutedEventHandler(OnTreeViewDirExpand);
+            return item;
+        }
+      
+        public BitmapImage GetFolderImage()
+        {
+            BitmapImage folderImage = new BitmapImage();
+            folderImage.BeginInit();
+            folderImage.UriSource = new Uri("C:\\Programming\\CSharpExplorer (WPF)\\FolderImage2.png");
+            folderImage.EndInit();
+
+            return folderImage;
+        }
+
+        public void CreateNameColumn(DirectoryInfo dir)
+        {
+            dirFileList.Items.Clear();
+
+            foreach (DirectoryInfo treeViewDirExpanded in dir.GetDirectories())
+            {
+                try
+                {
+                    dirFileList.Items.Add(new stackPanelParams()
+                    {
+                        ImageSource = GetFolderImage(),
+                        ImageHeight = 16,
+                        ImageWidth = 16,
+                        FolderLabel = treeViewDirExpanded.ToString(),
+                        FileCreationTime = treeViewDirExpanded.CreationTime,
+                        ItemTag = treeViewDirExpanded,
+                        FileType = "Folder"
+                       // CheckBoxShow= Visibility.Hidden
+                    });
+                  
+                }
+                catch (UnauthorizedAccessException ex)
+                {
+
+                }
+            }
+        }
+
+      //This populates the name column etc when the expand arrow in the treeview is clicked
+        public void OnTreeViewDirExpand(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem dirItem = e.Source as TreeViewItem;
+
+            if ((dirItem.Items.Count == 1) && (dirItem.Items[0] is string))
+            {
+                dirItem.Items.Clear();
+                dirFileList.Items.Clear();
+
+                DirectoryInfo expandedDir = null;
+                if (dirItem.Tag is DriveInfo)
+                    expandedDir = (dirItem.Tag as DriveInfo).RootDirectory;
+                if (dirItem.Tag is DirectoryInfo)
+                    expandedDir = (dirItem.Tag as DirectoryInfo);
+                try
+                {
+                    foreach (DirectoryInfo subDir in expandedDir.GetDirectories())
+                    {
+                        dirItem.Items.Add(CreateTreeItem(subDir));
+                    }
+                    CreateNameColumn(expandedDir);
+                   // dirItem.Selected += new RoutedEventHandler(OnTreeViewDirExpand);
+                }
+                catch { }
+            }
+
+            ResizeGridViewColumn(gvcName);
+            ResizeGridViewColumn(gvcFileType);
+            
+        }
+
+        public void TreeViewItem_Enter(object sender, RoutedEventArgs e)
+        {
+          ListViewItem item = e.Source as ListViewItem;
+
+          //dirFileList.Items.Add(new stackPanelParams()
+          //{            
+          //    CheckBoxShow = Visibility.Visible
+          //});
+                  
+
+         // stackPanelParams spp = new stackPanelParams();
+          
+          //spp.CheckBoxShow = Visibility.Visible;
+           // spp.FolderLabel = "hello";
+       
+        }
+
+        public void TreeViewItem_Leave(object sender, RoutedEventArgs e)
+        {
+            stackPanelParams obj = new stackPanelParams();
+            obj.CheckBoxShow = Visibility.Hidden;
+        }
+
+        //This captures single clicks on the drive tree and double clicks on the name column
+        public void TreeViewItem_Selected(object sender, RoutedEventArgs e)
+        {
+            ListViewItem selectedItem = e.Source as ListViewItem;            
+            TreeViewItem treeItem = e.Source as TreeViewItem;
+          
+            DirectoryInfo expandedDir = null;
+
+            if (treeItem != null)
+            {
+                if (treeItem.Tag is DriveInfo)
+                    expandedDir = (treeItem.Tag as DriveInfo).RootDirectory;
+                if (treeItem.Tag is DirectoryInfo)
+                    expandedDir = (treeItem.Tag as DirectoryInfo);
+            }
+            else
+            {
+                DirectoryInfo listItem = selectedItem.Tag as DirectoryInfo;
+                expandedDir = listItem;
+            }
+                
+            dirFileList.Items.Clear();
+
+            NativeMethods.SHFILEINFO info = new NativeMethods.SHFILEINFO();
+   
+            try
+            {               
+                //Create folders in list view
+                foreach (DirectoryInfo subDir in expandedDir.GetDirectories())
+                {
+                    dirFileList.Items.Add(new stackPanelParams()
+                    {
+                        ImageSource = GetFolderImage(),
+                        ImageHeight = 16,
+                        ImageWidth = 16,
+                        FolderLabel = subDir.ToString(),
+                        FileCreationTime = subDir.CreationTime,
+                        ItemTag = subDir,
+                        FileType = "Folder"
+                    });
+                }
+                //Create files in list view
+                foreach (FileInfo fileName in expandedDir.GetFiles())
+                {
+                    //FileInfo[] test = dirItems.GetFiles();
+                    ////Get file extension type
+                    uint dwFileAttributes = NativeMethods.FILE_ATTRIBUTE.FILE_ATTRIBUTE_NORMAL;
+                    uint uFlags = (uint)(NativeMethods.SHGFI.SHGFI_TYPENAME | NativeMethods.SHGFI.SHGFI_USEFILEATTRIBUTES);
+
+                    NativeMethods.SHGetFileInfo(fileName.ToString(), dwFileAttributes, ref info, (uint)Marshal.SizeOf(info), uFlags);
+                    ////
+
+                    ////Get file icon
+                    var sysicon = System.Drawing.Icon.ExtractAssociatedIcon(fileName.FullName);
+
+                    var bmpSrc = System.Windows.Interop.Imaging.CreateBitmapSourceFromHIcon(
+                                sysicon.Handle,
+                                System.Windows.Int32Rect.Empty,
+                                System.Windows.Media.Imaging.BitmapSizeOptions.FromEmptyOptions());
+                    sysicon.Dispose();
+                    ////
+               
+                    dirFileList.Items.Add(new stackPanelParams()
+                    {
+                        ImageSource = bmpSrc,
+                        ImageHeight = 16,
+                        ImageWidth = 16,
+                        FolderLabel = fileName.ToString(),
+                        FileCreationTime = fileName.CreationTime,
+                        FileType = info.szTypeName,
+                        ItemTag = fileName
+                    });
+
+                    ResizeGridViewColumn(gvcName);
+                    ResizeGridViewColumn(gvcFileType);                    
+                }                                          
+            }
+            catch { }
+        }
+
+        static class NativeMethods
+        {
+            [StructLayout(LayoutKind.Sequential)]
+            public struct SHFILEINFO
+            {
+                public IntPtr hIcon;
+                public int iIcon;
+                public uint dwAttributes;
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+                public string szDisplayName;
+                [MarshalAs(UnmanagedType.ByValTStr, SizeConst = 80)]
+                public string szTypeName;
+            };
+
+            public static class FILE_ATTRIBUTE
+            {
+                public const uint FILE_ATTRIBUTE_NORMAL = 0x80;
+            }
+
+            public static class SHGFI
+            {
+                public const uint SHGFI_TYPENAME = 0x000000400;
+                public const uint SHGFI_USEFILEATTRIBUTES = 0x000000010;
+            }
+
+            [DllImport("shell32.dll")]
+            public static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+        }
+
+        //The file type gridview column needs resizing to file the type name in 
+        //This needs to be done each time the gridcolumm is populated
+        private void ResizeGridViewColumn(GridViewColumn column)
+        {
+            if (double.IsNaN(column.Width))
+            {
+                column.Width = column.ActualWidth;
+            }
+            column.Width = double.NaN;
+        }
+    }
+
+
+    public class stackPanelParams : INotifyPropertyChanged
+    {
+        DateTime _fileCreationTime;
+        private Visibility visibility;
+        string filename;
+       
+        public double ImageWidth { get; set; }
+        public double ImageHeight { get; set; }
+        public BitmapSource ImageSource { get; set; }              
+        public string DriveLabel { get; set; }
+        public string FolderLabel { get; set; }
+        public object DriveInfoObj { get; set; }
+        public object ItemTag { get; set; }   
+        public object files { get; set; }
+      //  public Visibility CheckBoxShow { get; set; }
+
+        public Visibility CheckBoxShow
+        {
+            get
+            {
+                return visibility;
+            }
+            set
+            {
+                visibility = value;
+
+              OnPropertyChanged("CheckBoxShow");
+            }
+        }
+
+        public string FileType 
+        {
+            get { return filename; }
+            set
+            {
+             if (filename != value)
+                 {
+                     filename = value;
+                     OnPropertyChanged("FileType");               
+                 }
+            }
+        }
+      
+        public DateTime FileCreationTime
+        {
+            get
+            {
+                return _fileCreationTime;
+            }
+            set
+            {
+                _fileCreationTime = value;
+            }
+        }
+
+        public bool IsString(object value)
+        {
+            return value is string;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+
+    }
+
+    
+}
